8e6a262731bfebeda2a43fd3e0c5b637ee291fda
Boris Ribarski <bribarski7@gmail.com>
2019-03-15 15:28:41 +0200

    Added circle

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
- #include <SDL.h>
+#include <SDL.h>
 #include <stdio.h>
 #include <GL/gl.h>
+#include <math.h>
+
+void krasota(float a,float b){
+    glBegin(GL_TRIANGLES);
+    for(float i=0;i<=6.28;i+=0.1){
+        glBegin(GL_TRIANGLES);
+        glVertex2f(a+cos(i)*0.5,b+sin(i)*0.5);
+        glVertex2f(a+cos(i)*0.5,a+0.01);
+        glVertex2f(b+0.01,b+sin(i)*0.5);
+        glEnd();
+    }
+}
+
+void circle(float x,float y,float r){
+    int n=32;
+    float step=M_PI/(n/2);
+    glBegin(GL_TRIANGLES);
+    for(int i=0;i<n;i++){
+        float a=step*i;
+        float a2=step*(i+1);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(x+cos(a)*r,y+sin(a)*r);
+        glVertex2f(x+cos(a2)*r,y+sin(a2)*r);
+        glVertex2f(x,y);
+        glEnd();
+    }
+}
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);SDL_Window *window = SDL_CreateWindow(
-        "SDL2/OpenGL Demo", 30, 30, 640, 480,
+        "SDL2/OpenGL Demo", 30, 30, 480, 480,
         SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
         SDL_GL_CreateContext(window);
     glClearColor(0,0,1,1);
@@ -14,6 +41,8 @@ int main(int argc, char* argv[]) {
     }
     float x=0;
     float y=0;
+    float a=0.2;
+    float b=0.2;
     while (1) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -33,12 +62,9 @@ int main(int argc, char* argv[]) {
                 }
         }
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(x+0.3,y-0.2);
-        glVertex2f(x-0.3,y-0.2);
-        glVertex2f(x,y+0.4);
-        glEnd();
 
+        float r=0.2;
+        circle(x,y,r);
         SDL_GL_SwapWindow(window);
     }
     SDL_Quit();
