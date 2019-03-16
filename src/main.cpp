8a5ad720c8a906b04f8c0400f8a5b42f2a4ea182
Lachezar Ivanov <l.valentinov.ivanov@gmail.com>
2019-03-16 15:22:39 +0200

    Main cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -8,70 +8,15 @@
 #include <time.h>
 #include <GL/gl.h>
 #include <cmath>
-
 #include <assert.h>
-
 using namespace std;
-
-#include "Questions.cpp"
 
 float window_w=640;
 float window_h=480;
 
+#include "Questions.cpp"
+#include "Grafika.cpp"
 
-void krasota(float a,float b,float r){
-    glBegin(GL_TRIANGLES);
-    for(float i=0;i<=6.28;i+=0.1){
-        glBegin(GL_TRIANGLES);
-        glVertex2f(a+cos(i)*r,b+sin(i)*r);
-        glVertex2f(a+cos(i)*r,a+0.01);
-        glVertex2f(b+0.01,b+sin(i)*r);
-        glEnd();
-    }
-}
-
-void circle(float x,float y,float r){
-    int n=32;
-    float step=M_PI/(n/2);
-    glBegin(GL_TRIANGLES);
-    for(int i=0;i<n;i++){
-        float a=step*i;
-        float a2=step*(i+1);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(x+cos(a)*r,y+sin(a)*r*(window_w/window_h));
-        glVertex2f(x+cos(a2)*r,y+sin(a2)*r*(window_w/window_h));
-        glVertex2f(x,y);
-        glEnd();
-    }
-}
-
-void rectangle(float x,float y,float w,float h){
-    w /= 2;
-    h /= 2;
-    h*=(window_w/window_h);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(x - w, (y - h));
-    glVertex2f(x - w, (y + h));
-    glVertex2f(x + w, (y - h));
-    glVertex2f(x + w, (y + h));
-    glEnd();
-}
-
-void load_image(const char *filepath){
-    SDL_Surface *image = IMG_Load (filepath);
-    assert(image);
-    //cout<<image->format->format<<endl;
-    //cout<<SDL_PIXELFORMAT_RGBA8888<<endl;
-    //assert(image->format->format==SDL_PIXELFORMAT_RGBA8888);
-
-    GLuint texture_id;
-    glGenTextures(1,&texture_id);
-    glBindTexture(GL_TEXTURE_2D, texture_id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image->w, image->h,0,GL_RGBA,GL_UNSIGNED_BYTE,image->pixels);
-    SDL_FreeSurface(image);
-}
 
 float abs(float a,float b){
     if(a > b){
@@ -82,50 +27,12 @@ float abs(float a,float b){
     }
 }
 
-void show_symbol(char c, float x, float y, float h,float w){
-    float tw = 95*6;
-    h /= 2;
-    w /= 2;
-    int n=(c-32)*6;
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_TRIANGLE_STRIP);
-    glTexCoord2f(n/tw,1);
-    glVertex2f(x - w, (y - h));
-    glTexCoord2f(n/tw,0);
-    glVertex2f(x - w, (y + h));
-    glTexCoord2f((n+6)/tw,1);
-    glVertex2f(x + w, (y - h));
-    glTexCoord2f((n+6)/tw,0);
-    glVertex2f(x + w, (y + h));
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
-}
-
-void show_text(string text, float x, float y, int s){
-    float h = s * 10;
-    float w = s * 6;
-    h/=window_h/2;
-    w/=window_w/2;
-    for(size_t i=0;i<text.size();i++){
-        show_symbol(text[i], x+i*w, y, h,w);
-    }
-}
-void show_text_centered(string text, float x, float y, int s){
-    float h = s * 10.0;
-    float w = s * 6.0;
-    h/=window_h/2;
-    w/=window_w/2;
-    x -= (text.size() - 1) * w / 2;
-    for(size_t i=0;i<text.size();i++){
-        show_symbol(text[i], x+i*w, y, h,w);
-    }
-}
-
 int main(int argc, char* argv[]) {
+    vector<Category> cats = load_questions();
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow(
-        "SDL2/OpenGL Demo", 0, 30, window_w, window_h,
-        SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE|SDL_WINDOW_FULLSCREEN_DESKTOP);
+        "Catch me", 0, 30, window_w, window_h,
+        SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
 
     if (window == NULL)
     {
@@ -158,6 +65,9 @@ int main(int argc, char* argv[]) {
     bool right1 = false;
     bool up1 = false;
     bool down1 = false;
+
+    bool showq = false;
+    Question question;
 
     while (1)
     {
@@ -224,63 +134,73 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        if (left){
-            x -= sp;
-            if (x < -1 + r){
-                x = -1 + r;
+        if (!showq){
+            if (left){
+                x -= sp;
+                if (x < -1 + r){
+                    x = -1 + r;
+                }
             }
-        }
-        if (right){
-            x += sp;
-            if (x > 1 - r){
-                x = 1 - r;
+            if (right){
+                x += sp;
+                if (x > 1 - r){
+                    x = 1 - r;
+                }
             }
-        }
-        if (up == true){
-            y += sp;
-            if (y > 1 - 2*r){
-                y = 1 - 2*r;
+            if (up == true){
+                y += sp;
+                if (y > 1 - 2*r){
+                    y = 1 - 2*r;
+                }
             }
-        }
-        if (down == true){
-            y -= sp;
-            if (y < -1 + 2*r){
-                y = -1 + 2*r;
+            if (down == true){
+                y -= sp;
+                if (y < -1 + 2*r){
+                    y = -1 + 2*r;
+                }
+            }
+
+            if (left1 == true){
+                x1 -= sp;
+                if (x1 < -1 + w/2){
+                    x1 = -1 + w/2;
+                }
+            }
+            if (right1 == true){
+                x1 += sp;
+                if (x1 > 1 - w/2){
+                    x1 = 1 - w/2;
+                }
+            }
+            if (up1 == true){
+                y1 += sp;
+                if (y1 > 1 - w){
+                    y1 = 1 - w;
+                }
+            }
+            if (down1 == true){
+                y1 -= sp;
+                if (y1 < -1 + w){
+                    y1 = -1 + w;
+                }
             }
         }
 
-        if (left1 == true){
-            x1 -= sp;
-            if (x1 < -1 + w/2){
-                x1 = -1 + w/2;
-            }
-        }
-        if (right1 == true){
-            x1 += sp;
-            if (x1 > 1 - w/2){
-                x1 = 1 - w/2;
-            }
-        }
-        if (up1 == true){
-            y1 += sp;
-            if (y1 > 1 - w){
-                y1 = 1 - w;
-            }
-        }
-        if (down1 == true){
-            y1 -= sp;
-            if (y1 < -1 + w){
-                y1 = -1 + w;
-            }
-        }
-        if(abs(x,x1)<r+w/2 && abs(y,y1)<r+w/2){
-            break;
-        }
         glClear(GL_COLOR_BUFFER_BIT);
+
+        if(abs(x,x1)<r+w/2 && abs(y,y1)<r+w/2){
+            showq = true;
+            question = cats[0].packs[0].questions[0];
+        }
+
+        if (showq){
+            show_question(question);
+        }
+
         rectangle(x1, y1, w, h);
         circle(x,y,r);
-        show_text_centered("p",  x,  y, 1);
-        show_text_centered("PL22888", x1, y1, 1);
+        show_text_centered("P1",  x,  y, 1);
+        show_text_centered("P2", x1, y1, 1);
         SDL_GL_SwapWindow(window);
     }
     SDL_Quit();
